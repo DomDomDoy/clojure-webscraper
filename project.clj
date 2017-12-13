4379d0dd0f6a6a353df0d179069e1256da6287c8
Ashton Hellwig <ahellwig.dev@gmail.com>
2017-12-13 14:31:07 -0700

    forgotten additions

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject clojure-webscrapper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clojure-webscrapper "0.0.1"
+  :description "An attempt at http://masnun.com/2016/03/20/web-scraping-with-clojure.html"
   :url "http://github.com/ashellwig/clojure-webscrapper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
+                 [http-kit "2.1.18"]]
   :main ^:skip-aot clojure-webscrapper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_webscrapper/core.clj
+++ src/clojure_webscrapper/core.clj
@@ -1,6 +1,16 @@
 (ns clojure-webscrapper.core
-  (:gen-class))
+  (:require [net.cgrand.enlive-html :as html]
+            [org.httpkit.client :as http]))
 
+
+; Fetch HTML Response
+(defn get-dom
+  []
+  (html/html-snippet
+    (:body @(http/get "http://kat.cr/tv/" {:insecure? true}))))
+
+
+; Main Function
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
