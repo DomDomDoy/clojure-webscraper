fedb43997994b8b8577bbb0bbe5a93f8e0cba94d
Ashton Hellwig <ahellwig.dev@gmail.com>
2017-12-13 14:29:49 -0700

    Deps and resolves

---
+++ test/clojure_webscrapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-webscrapper.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-webscrapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
